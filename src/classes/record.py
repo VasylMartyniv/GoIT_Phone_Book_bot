911461af5ef6be69f7db6ebe88bc6eea3598966a
Vitalii1983-vr <vradvokat1@gmail.com>
2024-05-24 16:30:54 +0200

    Додано новий клас Birthday для валідації та роботи з датою народження, оновлено клас Record

--- src/classes/record.py
+++ src/classes/record.py
@@ -1,14 +1,45 @@
+from datetime import datetime
+
+# Базовий клас для полів запису
+class Field:
+    def __init__(self, value):
+        self.value = value  # Ініціалізація значення поля
+
+# Клас для зберігання дати народження з валідацією
+class Birthday(Field):
+    def __init__(self, value):
+        try:
+            # Перевірка формату дати і перетворення її в об'єкт datetime
+            self.value = datetime.strptime(value, '%d.%m.%Y').date()
+        except ValueError:
+            # Викидання помилки, якщо формат дати некоректний
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+
+    def __str__(self):
+        # Перетворення об'єкта дати в строку для виведення
+        return self.value.strftime('%d.%m.%Y')
+
+# Клас для зберігання інформації про контакт
 class Record:
     def __init__(self, name):
-        self.name = Name(name)  # Ініціалізуємо поле 'name' як об'єкт класу Name
-        self.phones = []  # Ініціалізуємо список телефонів для контакту
-        self.birthday = None  # Ініціалізуємо поле 'birthday' як None (відсутнє значення дати народження)
+        self.name = Name(name)  # Ініціалізація імені контакту як об'єкта класу Name
+        self.phones = []  # Ініціалізація списку телефонів для контакту
+        self.birthday = None  # Ініціалізація поля для дати народження як None
 
     def add_birthday(self, birthday):
-        self.birthday = Birthday(birthday)  # Додаємо (встановлюємо) день народження, перетворюючи в об'єкт класу Birthday
+        # Додавання дати народження, перетворюючи строку в об'єкт класу Birthday
+        self.birthday = Birthday(birthday)
 
     def change_birthday(self, new_birthday):
-        self.birthday = Birthday(new_birthday)  # Змінюємо існуючу дату народження на нову, перетворюючи в об'єкт класу Birthday
+        # Зміна існуючої дати народження на нову, перетворюючи строку в об'єкт класу Birthday
+        self.birthday = Birthday(new_birthday)
 
     def delete_birthday(self):
-        self.birthday = None  # Видаляємо дату народження, встановлюючи поле 'birthday' як None
+        # Видалення дати народження, встановлюючи поле 'birthday' як None
+        self.birthday = None
+
+    def __str__(self):
+        # Перетворення об'єкта Record в строку для виведення
+        phones = "; ".join(p.value for p in self.phones)
+        birthday = str(self.birthday) if self.birthday else "No birthday"
+        return f"Name: {self.name.value}, Phones: {phones}, Birthday: {birthday}"
